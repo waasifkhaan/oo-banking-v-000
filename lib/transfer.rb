61f2737eccc097b2312aa397695fac8df4db2d83
Wasif Khan <waasif.khaan@gmail.com>
2018-05-16 21:28:36 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
   end 
    
   def reverse_transfer
-    if self.status == "executed"
+    
     @sender.balance += amount 
     @receiver.balance -= amount
     self.status = "reversed"
