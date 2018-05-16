a4393dda2b4d1a966d3509c338631e087ecbbd0c
Wasif Khan <waasif.khaan@gmail.com>
2018-05-16 18:54:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,5 @@
 class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
+  attr_accessor :sender, :receiver, :amount, :status, :bank
   def initialize(sender, receiver = nil, amount = 0)
     @sender = sender
     @receiver = receiver
