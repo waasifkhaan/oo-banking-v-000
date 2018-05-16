52d58ab616a4723aae006847949de0e45c9320a8
Wasif Khan <waasif.khaan@gmail.com>
2018-05-16 20:50:34 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,6 +12,7 @@ class Transfer
     @sender.valid? && @receiver.valid?
   end 
   def execute_transaction
+    binding.pry
       if valid? && @status == "pending" 
         @sender.balance -= amount 
         @receiver.balance += amount
