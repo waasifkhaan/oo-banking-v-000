2f4fba24b5d73c238d6b5e6c4a8c89dfe77699b2
Wasif Khan <waasif.khaan@gmail.com>
2018-05-16 19:35:49 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,6 +16,7 @@ class Transfer
       @sender.balance - amount 
       @receiver.balance + amount
     else
+      
       "Transaction rejected. Please check your account balance."
       
   end 
